b43b455b5f2617beb2a3eb4ce17a2c526dd8d7d2
jhs512 <jangka512@gmail.com>
2021-11-07 16:37:53 +0900

    데이지 UI 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="bg-[#afafaf] h-[200px]">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo itaque
-      quasi molestiae repellendus natus similique vitae. Consectetur sapiente
-      soluta perferendis, ea recusandae minima ex libero, consequatur
-      reprehenderit, laborum odit neque!
-    </div>
+    <>
+      <button className="btn btn-primary">버튼1</button>
+    </>
   );
 }
 
